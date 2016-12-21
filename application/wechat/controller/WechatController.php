b576f3fe863e1708e9c086b6c59d1a1ee5d8649e
mskies <3792535@qq.com>
2016-12-21 18:59:59 +0800

    增加注释

--- application/wechat/controller/WechatController.php
+++ application/wechat/controller/WechatController.php
@@ -3,8 +3,10 @@ namespace app\wechat\controller;
 use think\Config;
 use think\Controller;
 class WechatController extends Controller{
-    protected $config;
-    public function __construct() {
+    protected $config;                  // 微信配置信息
+    
+    public function __construct(Request $request = null) {
+        parent::__construct($request);
         $this->config = Config::get('wechat');
     }
 }
