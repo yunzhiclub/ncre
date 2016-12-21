56d16d3ecf533db84423d09badcc0284ba2a0d2a
mskies <3792535@qq.com>
2016-12-21 19:01:15 +0800

    初始化API方法。

---
+++ application/api/controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+namespace app\api\controller;
+use think\Request;
+use app\model\UserModel;
+
+class UserController extends ApiController {
+    private $UserModel;
+
+    public function __construct(Request $request = null) {
+        parent::__construct($request);
+
+        $openid = Request::instance()->param('openid');
+        if (!UserModel::checkOpenidLength($openid)) {
+            $this->response(20002);     // openid长度不正确
+            return;
+        }
+
+        $UserModel = UserModel::getUserModelByOpenid($openid);
+    }
+
+    /**
+     * 设置身份证号码
+     * @param    integer                  $IdCardNum [description]
+     * @author 梦云智 http://www.mengyunzhi.com
+     * @DateTime 2016-12-21T16:51:06+0800
+     */
+    public function setIDCardNum($IdCardNum = 0) {
+        try {
+            // 校验身份证号
+            // 对UserModel的id_card_num赋值，并保存
+
+            
+            // 成功设置，返回空数组
+            return $this->response([]);
+
+        } catch (\Exception $e) {
+            $this->logException($e);
+        }
+    }
+
+    /**
+     * 设置是否接收推送消息
+     * @author 梦云智 http://www.mengyunzhi.com
+     * @DateTime 2016-12-21T18:56:09+0800
+     */
+    public function setIsReceiveMessage($isReceiveMessage = 0) {
+        try {
+
+            
+            // 成功设置，返回空数组
+            return $this->response([]);
+
+        } catch (\Exception $e) {
+            $this->logException($e);
+        }
+    }
+}
