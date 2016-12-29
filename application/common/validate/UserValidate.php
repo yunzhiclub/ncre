1be1e645c629e1bb8d7fb58a6f4b699261789d0d
liuxiqian <1454179583@qq.com>
2016-12-29 09:48:32 +0800

    增加checkIDCardNumLength验证信息

--- application/common/validate/UserValidate.php
+++ application/common/validate/UserValidate.php
@@ -5,8 +5,16 @@ use think\Validate;		//验证类
 class UserValidate extends validate
 {
     protected $rule = [
-        'id_card_num'        => 'require|max:18',
-        //'is_receive_message' => 'require|in:0,1',
+        ['id_card_num', 'checkIDCardNumLength:18', '身份证格式错误'],
     ];
 
+    protected function checkIDCardNumLength($IDCardNum, $rule) {
+        // 较验openid的长度
+        if (strlen($IDCardNum) === 18) {
+            return true;   
+        } else {
+            return '身份证只能是' . $rule . '位长度';
+        }
+        
+    }
 }
