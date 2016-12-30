9ad3879d3f8f2af634251098264d0de217900a7f
litian1218 <1181551049@qq.com>
2016-12-30 12:06:29 +0800

    增加清空表 修改为读取考场信息

--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -1,33 +1,38 @@
 <?php
 namespace app\index\controller;
 
+use think\Db;
 use think\Controller;
 use think\Request;
 use app\model\ScoreModel;
+use app\model\TestRoomModel;
 class IndexController extends Controller
 {
     // 文件上传提交并读取
     public function upload(Request $request)
     {
+        // 文件上传
         // 获取表单上传文件
-        $files = $request->file('file');
-        $item = [];
-
-        foreach ($files as $file ) {
-            // 移动到框架应用目录/runtime/uploads/ 目录下
-            $info = $file->move(ROOT_PATH . 'runtime' . DS . 'uploads');
-            // 上传成功则保存文件名
-            if ($info) {
-                $item[] = $info->getRealPath();
-                $title = $info->getSaveName();
-            } else {
-                // 上传失败获取错误信息
-                $this->error($file->getError());
-            }
+        $file = $request->file('file');
+        
+        if (empty($file)) {
+            $this->error('请选择上传文件');
+        }
+        // 移动到框架应用目录/runtime/uploads/ 目录下
+        $info = $file->move(ROOT_PATH . 'runtime' . DS . 'uploads');
+        // 上传成功则保存文件名
+        if ($info) {
+            $title = $info->getSaveName();
+        } else {
+            // 上传失败获取错误信息
+            $this->error($file->getError());
         }
         
+        // 文件读取
         // 实例化score
-        $ScoreModel = new ScoreModel;
+        // $ScoreModel = new ScoreModel;
+        // 实例化考场testroom
+        $TestRoomModel = new TestRoomModel;
 
         //dbase数据库的地址和文件名
         $dbf= RUNTIME_PATH.'uploads'. DS . $title;
@@ -40,7 +45,8 @@ class IndexController extends Controller
             //依次读取每一行数据
             for ($i = 1; $i <= $record_numbers; $i++) {
                 $row = dbase_get_record_with_names($db, $i);
-                $keys = ['ZJH', 'ZKZH', 'CJ', 'ZSBH'];
+                $keys = ['ZJH', 'ZKZH', 'KCH', 'PCH'];
+                // $keys = ['ZJH', 'ZKZH', 'CJ', 'ZSBH'];
                 foreach ($keys as $key) {
                     //检查是否存在$key
                     if (array_key_exists($key, $row)) {
@@ -50,17 +56,14 @@ class IndexController extends Controller
                 //合并数组
                 array_push($lists, $list);
 
-                // 测试用
-                if ($i === 4) {
-                    break;
-                }
             }
-            // var_dump($lists);
-            // 将数组循环存储全部
-            if ($ScoreModel->saveAll($lists)) {
+            // 清空数据表
+            $result = Db::execute('TRUNCATE table yunzhi_test_room');
+            // 成绩则存储$ScoreModel
+            if ($TestRoomModel->saveAll($lists)) {
                 return "批量保存成功";
             }else{
-                return $ScoreModel->getError();
+                return $TestRoomModel->getError();
             }
             dbase_close($db);
         }
